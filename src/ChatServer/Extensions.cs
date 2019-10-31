5e7825d3c465fbe0b1bb6f26d4456b8df6c33731
Lukasz Mieczkowski <lukasz.mieczkowski@ultrico.com>
2019-10-31 07:09:17 +0100

    Extract recipient (if exists) from string msg
    Expected string format: "@RECIPIENT:MESSAGE".

--- src/ChatServer/Extensions.cs
+++ src/ChatServer/Extensions.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ChatServer
 {
@@ -17,11 +18,40 @@ namespace ChatServer
         }
     }
 
-    internal static class StringExtensions
+    public static class StringExtensions
     {
         public static bool Contains(this string @this, string substring, StringComparison comparison)
         {
             return @this?.IndexOf(substring, comparison) >= 0; 
+        }
+
+        public static bool IsEmpty(this string @this)
+        {
+            return string.IsNullOrWhiteSpace(@this);
+        }
+
+        /// <summary>
+        /// Extracts a recipient name from the string value. The string value must be
+        /// formatted as follows: "@RECIPIENT:MESSAGE".
+        /// </summary>
+        /// <param name="this">Input string.</param>
+        /// <returns>RECIPIENT value from the string formatted as follows: "@RECIPIENT:MESSAGE".</returns>
+        public static string TryExtractRecipient(this string @this)
+        {
+            string recipient = null;
+
+            if(!@this.IsEmpty() && @this.StartsWith("@"))
+            {
+                var recipientRegEx = new Regex(@"@(\w+):");
+                var match = recipientRegEx.Match(@this.TrimStart());
+
+                if(match.Success)
+                {
+                    recipient = match.Groups[1].Value;
+                }
+            }
+
+            return recipient;
         }
     }
 }
